8eba998d9cd51629f1a6e640c5905c2d38ae7c81
shashiranjan84 <shashiranjan84@gmail.com>
2014-09-28 17:48:08 -0400

    updated TwilioRestClient

--- src/main/java/com/shashi/todo/helper/TwilioRestClientConfig.java
+++ src/main/java/com/shashi/todo/helper/TwilioRestClientConfig.java
@@ -2,9 +2,11 @@ package com.shashi.todo.helper;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.twilio.sdk.TwilioRestClient;
 
+@Configuration
 public class TwilioRestClientConfig {
 
 	@Value("#{systemEnvironment['TWILIO_SID']}")
